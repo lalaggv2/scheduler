689ee0d9f2fca8a12985c4ad097c1fd237ccc912
Lala <lalagg@hotmail.com>
2020-04-27 20:10:49 -0400

    functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,80 @@
+// -- GIVEN I am using a daily planner to create a schedule
+// -- WHEN I open the planner
+// -- THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// -- THEN I can enter an event
+// --  WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+// To create a scheduler that allows the user to add, modify and delete content
+
+// 1. I need to create a table in html to be used as schedule
+
+//  2. I need to check moment.js to add the dates information
+
+//  3. I need a way for people to modify the html table with new info
+
+// A function that get items modified.
+
+//  4. that new info need to be able to add more, modify and delete the items
+
+//  The scheduler needs to know the date (day of the week) and hours of the day
+
+//  first use a bootstrap model with list to design the appearance of the schedule
+
+// then with JS access it to modify it.
+
 var toDos = ["item 1", "item 2", "item 3"];
 
 function showToDos() {
-    document.createElement("div")
+    // document.createElement("div")
     console.log(toDos);
     //document.getElementById("todos");
 }
 
-showToDos();
 // I need a way to add new to dos
-function addToDos(todo) {
+function saveToDo(todo) {
     toDos.push(todo);
-    showToDos();
+    console.log(todo)
+    alert('saving')
 }
 
-addToDos("nothing");
-addToDos("dormir");
-// and a way to modify a todo
-function changeToDos(index, newToDo) {
-    toDos[index] = newToDo;
-    showToDos();
+function loadPlanner(start, end) {
+
+    const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    const currentHour = moment().format('HH');
+
+    $('#today').text(now);
+    for (var i = start; i <= end; i++) {
+        var row = $("<div>");
+        var hour = $(`<div>`);
+        var task = $(`<input id=${i} class="todo-item">`);
+        var save = $(`<button data-hour=${i}>`);
+
+        hour.text(`${i}:00`);
+
+        save.text("save");
+        save.on("click", function (e) {
+            console.log($(e.target).text())
+            const buttonHour = $(e.target).attr('data-hour')
+            saveToDo({
+                hour: buttonHour,
+                task: $(`#${buttonHour}`).val()
+            });
+        })
+        row.append(hour);
+        row.append(task);
+        row.append(save);
+        $("#planner").append(row)
+    }
+    showToDos()
 }
 
-changeToDos(0, "changed");
-changeToDos(3, "nada");
-// and a way to delete to dos
-function deleteToDo(index) {
-    toDos.splice(index, 1);
-    showToDos();
-}
-
-deleteToDo(0);
+loadPlanner(9, 17);
