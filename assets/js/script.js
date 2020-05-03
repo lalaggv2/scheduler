b350e0fdc0b35c01350fda826fd2e35365a4e5bb
Lala <lalagg@hotmail.com>
2020-05-03 17:45:30 -0400

    commit bs jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-
 // To create a scheduler that allows the user to add, modify and delete content
 
 // 1. I need to create a table in html to be used as schedule
@@ -34,56 +33,54 @@
 var toDos = ["item 1", "item 2", "item 3"];
 
 function showToDos() {
-    // document.createElement("div")
-    console.log(toDos);
-    //document.getElementById("todos");
+  // document.createElement("div")
+  console.log(toDos);
+  //document.getElementById("todos");
 }
 
 // I need a way to add new to dos
 function saveToDo(hour, task) {
-    toDos.push(task);
-    console.log(task);
-    localStorage.setItem("hour", hour);
-    localStorage.setItem("task", task);
-    let getItem = localStorage.getItem("task");
-    console.log(getItem);
-    // alert('saving')
+  toDos.push(task);
+  console.log(task);
+  localStorage.setItem("hour", hour);
+  localStorage.setItem("task", task);
+  let getItem = localStorage.getItem("task");
+  console.log(getItem);
+  // alert('saving')
 }
 
 function loadPlanner(start, end) {
+  const now = moment().format("MMMM Do YYYY, h:mm:ss a");
+  const currentHour = moment().format("HH");
 
-    const now = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const currentHour = moment().format('HH');
+  $("#today").text(now);
+  for (var i = start; i <= end; i++) {
+    var row = $("<div>");
+    var hour = $(`<div>`);
+    var task = $(`<input id=${i} class="todo-item">`);
+    var save = $(`<button data-hour=${i}>`);
 
-    $('#today').text(now);
-    for (var i = start; i <= end; i++) {
-        var row = $("<div>");
-        var hour = $(`<div>`);
-        var task = $(`<input id=${i} class="todo-item">`);
-        var save = $(`<button data-hour=${i}>`);
+    hour.text(`${i}:00`);
 
-        hour.text(`${i}:00`);
-
-        save.text("save");
-        save.on("click", function (e) {
-            console.log($(e.target).text())
-            const buttonHour = $(e.target).attr('data-hour')
-            saveToDo(buttonHour, $(`#${buttonHour}`).val());
-        })
-        row.append(hour);
-        row.append(task);
-        row.append(save);
-        $("#planner").append(row)
-    }
-    showToDos()
+    save.text("save");
+    save.on("click", function (e) {
+      console.log($(e.target).text());
+      const buttonHour = $(e.target).attr("data-hour");
+      saveToDo(buttonHour, $(`#${buttonHour}`).val());
+    });
+    row.append(hour);
+    row.append(task);
+    row.append(save);
+    $("#planner").append(row);
+  }
+  showToDos();
 }
 
 loadPlanner(9, 17);
 
 //save to local storage and get from local storage
 function savePlanner() {
-    var hoursTask = localStorage.getItem("hour", JSON.stringify(task));
-    //const data = JSON.parse(localStorage.getItem("todo")); (
-    localStorage.setItem("hour", task);
+  var hoursTask = localStorage.getItem("hour", JSON.stringify(task));
+  //const data = JSON.parse(localStorage.getItem("todo")); (
+  localStorage.setItem("hour", task);
 }
-
